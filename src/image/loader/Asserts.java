bbb9f9db7e364a3b86594d8e5f3c66ee03d588e1
unknown <hasinthashehan768@gmail.com>
2019-10-16 01:24:03 +0530

    .

---
+++ src/image/loader/Asserts.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package image.loader;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+
+
+
+
+
+
+/**
+ *
+ * @author SHEHAN
+ */
+public class Asserts {
+    private static BufferedImage img;
+    public static BufferedImage walk;
+    public static SpriteSheet sheet;
+    
+    public static void init(){
+        try {
+            img = gfx.loadImage("./res/spritesheet.png");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
+    public static void Loadassert(){
+        sheet = new SpriteSheet(img);
+        walk = sheet.crop(0,0,100,100);
+        
+    }
+}
